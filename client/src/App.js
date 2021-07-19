eccffa1d3201ee4d64ad28603acdb60dfe0049d2
Nathaniel Duya <nathanduya@gmail.com>
2021-07-18 20:00:54 -0700

    Cleaned up client folder Added basic registration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,12 @@
 import { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Registration from './pages/registration';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!data ? "Loading..." : data}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Registration />
     </div>
   );
 }
