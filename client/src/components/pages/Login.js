c935669967c61c8c9b0610097810f63ae711e807
Nathaniel Duya <nathanduya@gmail.com>
2021-07-28 23:26:47 -0700

    Created user registration will change to async/await later

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,17 +1,16 @@
-
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from "../Navbar";
 
 function Login(){
+
     return(
         <>
-            <Navbar />
             <form method='POST' action='/users/register' enctype="application/x-www-form-urlencoded">
-                <div class='form-field'>
+                <div className='form-field'>
                     <label>Username</label>
                     <input id="username" type="text" placeholder="Username" name="username" required />
                 </div>
-                <div class='form-field'>
+                <div className='form-field'>
                     <label>Password</label>
                     <input id="password" type="text" placeholder="Password" name="password" required />
                 </div>
--- client/src/components/pages/Registration.js
+++ client/src/components/pages/Registration.js
@@ -1,33 +1,59 @@
+import { useRef, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
 
-import { Link } from 'react-router-dom'
 function Registration(){
+    const usernameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const cpasswordRef = useRef();
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:3001/users/register', {
+            username: usernameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            cpassword: cpasswordRef.current.value
+        }).then((res) => {
+            if(res.data.success){
+                history.push("/login");
+            }
+            else{
+                window.location.reload(false);
+            }
+        });
+    };
+
     return(
         <>
-            <form method='POST' action='/users/register' enctype="application/x-www-form-urlencoded">
-                <div class='form-field'>
+            <form onSubmit={handleSubmit}>
+                <div className='form-field'>
                     <label>Username</label>
-                    <input id="username" type="text" placeholder="Username" name="username" required />
+                    <input id="username" type="name" placeholder="Username" ref={usernameRef} required />
                 </div>
-                <div class='form-field'>
+                <div className='form-field'>
                     <label>Email</label>
-                    <input id="email" type="text" placeholder="Email" name="email" required />
+                    <input id="email" type="email" placeholder="Email" ref={emailRef} required />
                 </div>
-                <div class='form-field'>
+                <div className='form-field'>
                     <label>Password</label>
-                    <input id="password" type="text" placeholder="Password" name="password" required />
+                    <input id="password" type="text" placeholder="Password" ref={passwordRef} required />
                 </div>
-                <div class='form-field'>
+                <div className='form-field'>
                     <label>Confirm Password</label>
-                    <input id="cpassword" type="text" placeholder="Confirm Password" name="cpassword" required />
+                    <input id="cpassword" type="text" placeholder="Confirm Password" ref={cpasswordRef} required />
                 </div>
 
                 <div>
                     <Link to='/login'>
                         Have An Account?
                     </Link>
-                    <button type="submit">Submit</button>
-
                 </div>
+                <button type="submit">Submit</button>
+
             </form>
         </>
         
