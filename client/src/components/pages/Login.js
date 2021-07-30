019d619b31378be355a54c0980c81c98bc0e9b4f
Nathaniel Duya <nathanduya@gmail.com>
2021-07-29 18:36:36 -0700

    Created user login in frontend and backend to database without sessions

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,18 +1,40 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useRef, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 function Login(){
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:3001/users/login', {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value
+        })
+        .then((res) => {
+            if(res.data.success){
+                console.log(res.data);
+                history.push("/signup");
+            }
+            else{
+                window.location.reload(false);
+            }
+        });
+    }
 
     return(
         <>
-            <form method='POST' action='/users/register' enctype="application/x-www-form-urlencoded">
+            <form onSubmit={handleSubmit}>
                 <div className='form-field'>
                     <label>Username</label>
-                    <input id="username" type="text" placeholder="Username" name="username" required />
+                    <input id="username" type="text" placeholder="Username" ref={usernameRef} required />
                 </div>
                 <div className='form-field'>
                     <label>Password</label>
-                    <input id="password" type="text" placeholder="Password" name="password" required />
+                    <input id="password" type="text" placeholder="Password" ref={passwordRef} required />
                 </div>
 
                 <div id='login-options'>
--- server/routes/users.js
+++ server/routes/users.js
@@ -91,11 +91,53 @@ router.post('/register', (req, res, next) => {
     else{
         res.status(200).json({success: false, message: "Invalid inputs", redirectURL: "/signup"});
     }
-}
+});
 
-// router.post('/login', (req, res, next) => {
-//     next();
-// });
-);
+router.post('/login', (req, res, next) => {
+    let username = req.body.username;
+    let password = req.body.password;
 
+    let userId;
+    let baseSQL = "SELECT id, username, password FROM users WHERE username=?;"
+
+    if(isUserValid(username) && isPasswordSecure(password)){
+        db.execute(baseSQL, [username])
+        .then(([results, fields]) => {
+            if(results && results.length == 1){
+                userId = results[0].id;
+                return bcrypt.compare(password, results[0].password);
+            }
+            else{
+                throw new UserError(
+                    "Invalid Username and/or Password",
+                    "/login",
+                    200
+                )
+            }
+        })
+        .then((passwordsMatch) => {
+            if(passwordsMatch){
+                return res.status(201).json({success: true, message: "User login successful", redirect: "/login", id: userId});
+            }
+            else{
+                throw new UserError(
+                    "Invalid Username and/or Password",
+                    "/login",
+                    200
+                )
+            }
+        })
+        .catch((err) => {
+            if(err instanceof UserError){
+                return res.status(err.getStatus()).json({success: false, message: err.getMessage(), redirectURL: err.getRedirectURL()});
+            }
+            else{
+                next(err);
+            }
+        })
+    }
+    else{
+        res.status(200).json({success: false, message: "Invalid inputs", redirectURL: "/login"});
+    }
+});
 module.exports = router;
