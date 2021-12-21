900e71843c68690a639a671a000823a0d472ed29
Nathaniel Duya <nathanielduya@yahoo.com>
2021-12-21 13:15:23 -0800

    Heroku fix

--- server/app.js
+++ server/app.js
@@ -20,12 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+//app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Routes
 app.use('/users', usersRouter);
 app.use('/budget', budgetRouter);
 
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname, '../client/build/index.html')); });
 const port = process.env.PORT || 3030;
 app.listen(port, () => {
     console.log("Listening on port " + port);
