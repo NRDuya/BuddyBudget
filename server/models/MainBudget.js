e4825277532c0de3758722ebe4a8201a85157465
Nathaniel Duya <nathanielduya@yahoo.com>
2021-12-21 18:52:06 -0800

    Refactored main budget

--- server/models/MainBudget.js
+++ server/models/MainBudget.js
@@ -2,27 +2,14 @@ var db = require('../config/database');
 const MainBudgetModel = {};
 
 MainBudgetModel.create = (category, expense, userId) => {
-    let baseSQL = "INSERT INTO mainBudget (category, expense, fk_userid) VALUES (?, ?, ?);"
-    db.execute(baseSQL, [category, expense, fk_userid])
-     .then(([results, fields]) => {
-        if(results && results.affectedRows){
-            return res.status(201).json({success: true, message: "Budget creation successful", redirect: "/"});
-        }
-        else{
-            throw new UserError(
-                "Server Error, budget could not be created",
-                "/",
-                500);
-        }
-     })
-     .catch((err) => {
-        if(err instanceof UserError){
-            return res.status(err.getStatus()).json({success: false, message: err.getMessage(), redirectURL: err.getRedirectURL()});
-        }
-        else{
-            next(err);
-        }
-    })
+    const baseSQL = "INSERT INTO mainBudget (category, expense, fk_userid) VALUES (?, ?, ?);"
+    db.execute(baseSQL, [category, expense, userId])
+        .then(([results, fields]) => {
+            if(results && results.affectedRows){
+                return Promise.resolve(1);
+            } else return Promise.reject(-1);
+        })
+        .catch((err) => Promise.reject(err))
 }
 
 MainBudgetModel.get = (userId) => {
@@ -31,10 +18,30 @@ MainBudgetModel.get = (userId) => {
         .then(([results, fields]) => {
             if(results && results.length){
                 return Promise.resolve(results);
+            } else return Promise.resolve(-1);
+        })
+        .catch((err) => Promise.reject(err))
+}
+
+MainBudgetModel.edit = (category, expense, budgetId) => {
+    const baseSQL = "UPDATE mainBudget SET category = ?, expense = ? WHERE id = ?;"
+    db.execute(baseSQL, [category, expense, budgetId])
+        .then(([results, fields]) => {
+            if(results && results.affectedRows){
+                return Promise.resolve(1);
+            } else return Promise.reject(-1);
+        })
+        .catch((err) => Promise.reject(err))
+}
+
+MainBudgetModel.delete = (budgetId) => {
+    let baseSQL = "DELETE FROM mainBudget where id = ?;";
+    db.execute(baseSQL, [budgetId])
+        .then(([results, fields]) => {
+            if(results && results.affectedRows){
+                return Promise.resolve(1);
             }
-            else{
-                return Promise.resolve(-1);
-            }
+            else return Promise.reject(-1);
         })
         .catch((err) => Promise.reject(err))
 }
