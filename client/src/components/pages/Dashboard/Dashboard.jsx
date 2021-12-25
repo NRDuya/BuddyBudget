751a55211ce6f4a7ac43967c55d66b02020da0bd
Nathaniel Duya <nathanielduya@yahoo.com>
2021-12-24 22:15:51 -0800

    Messing around with calendar

--- client/src/components/pages/Dashboard/Dashboard.jsx
+++ client/src/components/pages/Dashboard/Dashboard.jsx
@@ -3,9 +3,13 @@ import Navbar from '../../Navbar';
 import MainBudget from './MainBudget/MainBudget';
 import DashboardBudget from './DashboardBudget';
 
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 function Dashboard() {
     const [shownBudget, setShownBudget] = useState(null);
-    
+    const [date, setDate] = useState(new Date());
+
     const handleBudgetClick = (type) => {
         setShownBudget(type);
     }
@@ -13,10 +17,20 @@ function Dashboard() {
     const handlExitBudgetClick = () => {
         setShownBudget(null);
     }
+
+    const handleCalendar = (value) => {
+        const month = value.getMonth();
+        const year = value.getYear();
+        console.log(month + ' ' + year)
+        console.log(value);
+        setDate(value);
+    }
     
     return (
         <>
             <Navbar />
+            {/* <input type="date" id="startDate" name="startDate" ></input> */}
+            <Calendar maxDetail="year" prev2Label={null} next2Label={null} onChange={handleCalendar} />
             <div className='app-container'>  
                 <DashboardBudget type={ 'variable' } handleBudgetClick={ handleBudgetClick }/>  
                 <DashboardBudget type={ 'fixed' } handleBudgetClick={ handleBudgetClick }/>  
