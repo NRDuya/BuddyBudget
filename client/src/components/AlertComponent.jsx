90fdf4388fb5fb65cb3b4c8366ccc63b0db4498f
Nathaniel Duya <nathanielduya@yahoo.com>
2022-06-29 09:29:16 -0700

    Added disappearing component to alert

--- client/src/components/AlertComponent.jsx
+++ client/src/components/AlertComponent.jsx
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
 import Alert from 'react-bootstrap/Alert';
 
 function AlertComponent({ alert, setAlert }){
+
     const closeAlert = () => {
         const newAlert = {...alert};
         newAlert.show = false;
         setAlert(newAlert);
     }
+
+    useEffect(() => {
+        if (alert.disappear) {
+            const timeId = setTimeout(() => {
+                closeAlert();
+              }, 5000)
+          
+              return () => {
+                clearTimeout(timeId)
+              }
+        }
+    }, []);
+
     return(
         <>
             <Alert variant={alert.type} onClose={closeAlert} dismissible>
