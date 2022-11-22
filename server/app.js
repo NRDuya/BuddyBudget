88b2c428940a94ab0c2db6684658fb8a6757356f
Nathaniel Duya <nathanielduya@yahoo.com>
2022-11-21 20:44:00 -0800

    Clean up project

--- server/app.js
+++ server/app.js
@@ -16,17 +16,11 @@ const summaryBudgetRouter = require('./routes/summaryBudgetRoute');
 
 const app = express();
 
-app.use(cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Routes
 app.use('/users', usersRouter);
@@ -37,7 +31,7 @@ app.use('/incomeBudget', incomeBudgetRouter);
 app.use('/monthlyBudget', monthlyBudgetRouter);
 app.use('/summaryBudget', summaryBudgetRouter);
 
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../client/build/index.html')); });
+//app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../client/build/index.html')); });
 app.use((req, res) => { res.status(404).json({ message: 'Route Not Found' }); });
 
 const port = process.env.PORT || 3030;
